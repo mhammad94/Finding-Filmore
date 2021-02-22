f91549c9d9a2669408a4ccbd7cde74e6fa94a7d6
mhammad94 <muhammadhammad.94@gmail.com>
2021-02-22 14:00:44 +0500

    First Commit

---
+++ script.js
@@ -0,0 +1,66 @@
+'use strict'
+
+/// Recent Trips Slider (Start)
+// Elements 
+
+const recentSliderNextbtn = document.querySelector('.recent-trips-img-controls-next');
+const recentSliderPrevbtn = document.querySelector('.recent-trips-img-controls-previous');
+const recentSliderimg = document.querySelector('.recent-trips-img');
+const recentSliderinnerImg = document.querySelector('.recent-trips-slider-images-inner');
+
+
+
+let firstSlide = 1;
+let lastSlide = 1;
+
+const recentSlideChange = function (firstNo, lastNo) {
+    const nextSlide = document.querySelector(`.recent-trips-content-slide-main-${firstNo}`);
+    const prevSlide = document.querySelector(`.recent-trips-content-slide-main-${lastNo}`);
+    recentSliderinnerImg.src = `Images/Img--${firstNo}.png`
+    nextSlide.classList.remove('display-None');
+    prevSlide.classList.add('display-None')
+
+};
+
+const NextSlideChange = function () {
+    firstSlide++
+    if (firstSlide <= 6) {
+        recentSlideChange(firstSlide, lastSlide);
+        lastSlide = firstSlide;
+    } else if (firstSlide >= 6) {
+        firstSlide = 1;
+        lastSlide = 6;
+        recentSlideChange(firstSlide, lastSlide);
+        lastSlide = firstSlide;
+        console.log(firstSlide, lastSlide)
+    }
+};
+
+const PrevSlideChange = function () {
+    firstSlide--
+    if (firstSlide > 0) {
+        recentSlideChange(firstSlide, lastSlide);
+        lastSlide = firstSlide;
+    } else if (firstSlide <= 0) {
+        firstSlide = 6;
+        lastSlide = 1;
+        recentSlideChange(firstSlide, lastSlide);
+        lastSlide = firstSlide;
+    }
+};
+
+let recentSliderDeg = 0;
+recentSliderNextbtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    recentSliderDeg -= 60;
+    recentSliderimg.style.transform = `rotate(${recentSliderDeg}deg)`;
+    NextSlideChange();
+});
+
+recentSliderPrevbtn.addEventListener('click', function (e) {
+    e.preventDefault;
+    recentSliderDeg += 60;
+    recentSliderimg.style.transform = `rotate(${recentSliderDeg}deg)`;
+    PrevSlideChange();
+});
+
